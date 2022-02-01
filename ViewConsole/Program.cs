3523dada0ace5f6fd3f42779c437d06be603dee9
Danya Provilskyi <65830545+Danila-cloud@users.noreply.github.com>
2022-02-01 21:56:20 +0200

    Добавьте файлы проекта.

---
+++ ViewConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ViewConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
