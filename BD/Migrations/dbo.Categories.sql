b5e16377a8621adee1506f1af415015818cbffd8
Danya Provilskyi <65830545+Danila-cloud@users.noreply.github.com>
2022-02-08 16:30:15 +0200

    add Categories, Products migrations

---
+++ BD/Migrations/dbo.Categories.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Categories] (
+    [Id]   UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
+    [Name] NVARCHAR (50)    NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ BD/Migrations/dbo.Products.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [Id]          UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
+    [Name]        NVARCHAR (50)    NOT NULL,
+    [Price]       MONEY            DEFAULT ((0)) NULL,
+    [Description] NVARCHAR (100)   NULL,
+    [Quantity]    INT              DEFAULT ((0)) NOT NULL
+);
+
